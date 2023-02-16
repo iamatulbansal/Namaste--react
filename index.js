92e6e5f2ca255a69fee12963b63a685759162e36
ATUL BANSAL <86524139+iamatulbansal@users.noreply.github.com>
2023-02-16 18:36:45 +0000

    11feb test questions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Testing")
+/** Write a program to solve this problem. 
+
+let data1 = { name: “sachin”, address: “surat”, pincode: 395865 }
+let data2 = { fname: “Tendulkar”, lname: "Ramesh", phoneno: "3213213213" }
+
+ Want this array as a final result, 
+[{name: "sachin", fname: "tendulkar"}, {address: "surat", lname: "Ramesh"}, {pincode: 395865, phoneno: "3213213213"}];
+ 
+Note: Don’t use any inbuilt es6 later javascript functions.
+Make a create react app or make a simple js file and run the code.
+  */
+
+let data1 = { name: “sachin”, address: “surat”, pincode: 395865 }
+let data2 = { fname: “Tendulkar”, lname: "Ramesh", phoneno: "3213213213" }
+
+
