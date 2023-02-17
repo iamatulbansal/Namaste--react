d1d3c04022dacc0e95461d6f05bdd229b76ba451
ATUL BANSAL <86524139+iamatulbansal@users.noreply.github.com>
2023-02-17 21:48:52 +0000

    Solved exam question Write a program to solve this problem

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-/** Write a program to solve this problem. 
+/** 
+Write a program to solve this problem. 
 
-let data1 = { name: “sachin”, address: “surat”, pincode: 395865 }
-let data2 = { fname: “Tendulkar”, lname: "Ramesh", phoneno: "3213213213" }
+let data1 = { name: "sachin", address: "surat", pincode: 395865 }
+let data2 = { fname: "Tendulkar", lname: "Ramesh", phoneno: 3213213213 }
 
  Want this array as a final result, 
-[{name: "sachin", fname: "tendulkar"}, {address: "surat", lname: "Ramesh"}, {pincode: 395865, phoneno: "3213213213"}];
+[
+  {name: "sachin", fname: "tendulkar"}, 
+  {address: "surat", lname: "Ramesh"}, 
+  {pincode: 395865, phoneno: "3213213213"}
+];
  
 Note: Don’t use any inbuilt es6 later javascript functions.
-Make a create react app or make a simple js file and run the code.
+make a simple js file and run the code.
   */
 
-let data1 = { name: “sachin”, address: “surat”, pincode: 395865 }
-let data2 = { fname: “Tendulkar”, lname: "Ramesh", phoneno: "3213213213" }
+ let data1 = { name: "sachin", address: "surat", pincode: 395865 };
+ let data2 = { fname: "Tendulkar", lname: "Ramesh", phoneno: 3213213213 };
 
 
+ function myFun(d1,d2){
+   let finalResult=[]
+   for(const key in d1){
+     finalResult.push({
+       [key]:data1[key],
+       [Object.keys(data2)[finalResult.length]]:data2[Object.keys(data2)[finalResult.length]]            })
+   }
+        return finalResult;
+   }
+console.log(myFun(data1,data2))
