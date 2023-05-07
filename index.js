7278cdc4b5a24c86ad7b9f790a3399e1fcec684e
ATUL BANSAL <86524139+iamatulbansal@users.noreply.github.com>
2023-05-08 00:23:09 +0530

    solve object maping problem

--- index.js
+++ index.js
@@ -15,17 +15,11 @@ Note: Don’t use any inbuilt es6 later javascript functions.
 make a simple js file and run the code.
   */
 
- let data1 = { name: "sachin", address: "surat", pincode: 395865 };
- let data2 = { fname: "Tendulkar", lname: "Ramesh", phoneno: 3213213213 };
+let data1 = { name: "sachin", address: "surat", pincode: 395865 };
+let data2 = { fname: "Tendulkar", lname: "Ramesh", phoneno: 3213213213 };
 
+const result = Object.keys(data1).map((key, index) => {
 
- function myFun(d1,d2){
-   let finalResult=[]
-   for(const key in d1){
-     finalResult.push({
-       [key]:data1[key],
-       [Object.keys(data2)[finalResult.length]]:data2[Object.keys(data2)[finalResult.length]]            })
-   }
-        return finalResult;
-   }
-console.log(myFun(data1,data2))
+  return ({ [key]: data1[key], [Object.keys(data2)[index]]: data2[Object.keys(data2)[index]] })
+})
+console.log(result)
