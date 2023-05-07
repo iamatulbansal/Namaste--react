1f04f760fd3f2511fe3ee21584649bb1a35c7ba2
ATUL BANSAL <86524139+iamatulbansal@users.noreply.github.com>
2023-05-08 03:42:13 +0530

    move script to the seprate file and first class wrapup

---
+++ App.js
@@ -0,0 +1,26 @@
+const heading1 = React.createElement(
+    "h1",
+    { id: "content1", className: "heading" },
+    "Heading1"
+);
+const heading2 = React.createElement(
+    "h1",
+    { id: "content2", className: "heading" },
+    "Heading2"
+);
+// *this is take three argument first is html tag second is props=attribute and third is children element
+const container = React.createElement(
+    "div",
+    {
+        id: "container",
+        className: "container",
+    },
+    [heading1, heading2]
+    //*wrap all heading inside a container div pass all element inside container tag
+);
+//   console.log(heading);
+// * Heading is not a html tag it is a plain javascript Object in react
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//   console.log(root);
+// * Passing a react element inside the root
+root.render(container);
