849a7fc71699a33ccac282c7c5fd11e14ca99eb0
ATUL BANSAL <86524139+iamatulbansal@users.noreply.github.com>
2023-05-08 23:57:54 +0530

    website planning

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ const HeadingComponent = () => {
     return (
         <div>
             <Title />
+            { }
             <h1>Namaste React Functional components</h1>
             <h2>This is a Heading 2</h2>
         </div>
@@ -14,3 +15,33 @@ const HeadingComponent = () => {
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<HeadingComponent />);
+
+
+/**
+ * *Day-4-classes
+ * todo: When you create a project or webApp First Do proper planning and structure layout 
+ * *AppLayout-planning
+ * Header-
+ *       - logo
+ *       - nav items(right side)
+ *       - cart (right side)
+ * Body-  
+ *      -search-bar
+ *      -RestaurantList
+ *       -RestaurantCard 
+ *        -image  
+ *        -name  
+ *        -rating  
+ *        -cuisines  
+ * 
+ * Footer-
+ *        -Link
+ *        -Copyright  
+ * 
+ * 
+ * 
+ * todo: when we use multi element inside our components then must be wrap up inside single element JSX expressions must have one parent element
+ * 
+ * 
+ *  
+ * */
