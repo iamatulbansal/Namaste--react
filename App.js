8e2260231a120822cb32ce5a53664c4602b01688
ATUL BANSAL <86524139+iamatulbansal@users.noreply.github.com>
2023-05-08 17:03:55 +0530

    add key and babelrc file and plug in for remove console.log and other error from console

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
-import React from "react";
-import ReactDOM from 'react-dom/client';
 
-
-
-/*** 
+/***
  * *PARCEL-BUNDLER
  * CREATE-LIVE-SERVER
  * HMR-HOT MODULE REPLACEMENT
  * FILE WATCHER ALGORITHM
- * AUTOMATIC CLEAR CONSOLE.LOG
+ * SETUP YOUR APP WITH BABEL PLUGIN TO (.babelrc) AUTOMATIC CLEAR CONSOLE.LOG
  * CONSISTENT HASHING ALGORITHM
  * FILE MINIFY
  * HTTPS ON DEVELOPMENT ENVIRONMENT USING NPX PARCEL INDEX.HTML --HTTPS
@@ -17,31 +13,34 @@ import ReactDOM from 'react-dom/client';
  * OLDER BROWSERS VERSION SUPPORTS
  * COMPRESSION
  * IMAGE OPTIMIZATION AUTOMATICALLY
- * CACHING WHILE DEVELOPMENT 
+ * CACHING WHILE DEVELOPMENT
  * SUPER FAST BUILD ALGORITHM
  * ZERO CONFIG
- * 
+ * TREE SHAKING - REMOVING UN-WANTED CODE
+ *
  * *OTHER IMPORTANT NOTE
- * USE YOUR INDEX.HTML FILE IN THE START AND BUILD COMMAND IN YOUR STARTER SCRIPT 
+ * USE YOUR INDEX.HTML FILE IN THE START AND BUILD COMMAND IN YOUR STARTER SCRIPT
  * REMOVE {main:"App.js"} <<< remove this line from package.json while using parcel
  * ADD type="module" <<<ADD INSIDE YOUR SCRIPT TAG IN INDEX.HTML FILE
- * 
+ *
  * *OTHER IMPORTANT NOTE
  * ^2.8.2 THIS SIGN USE FOR AUTOMATIC VERSION UPDATE
  * ~2.8.2 THIS SIGN USE FOR PATCH UPDATE
  *  2.8.2 NOT ANY SING THEN YOUR PROJECT WORKING ONLY SPECIFIC VERSION
  * PUSH YOUR PACKAGE-LOCK.JSON FILE TO YOUR GIT REPO SO WHEN WE DEPLOY YOUR APP THEN SERVER KNOW HWT VERSION DO YOU USE IN YOUR PROJECT DON'T DELETE YOUR PACKAGE-LOCK.JSON
- * 
-  */
+ *
+ */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const heading1 = React.createElement(
     "h1",
-    { id: "content1", className: "heading" },
+    { id: "content1", className: "heading", key: "h1" },
     "Heading1"
 );
 const heading2 = React.createElement(
-    "h1",
-    { id: "content2", className: "heading" },
+    "h2",
+    { id: "content2", className: "heading", key: "h2" },
     "Heading2"
 );
 // *this is take three argument first is html tag second is props=attribute and third is children element
@@ -50,11 +49,12 @@ const container = React.createElement(
     {
         id: "container",
         className: "container",
+        key: "div",//We will Pass Kay inside props object like this
     },
     [heading1, heading2]
     //*wrap all heading inside a container div pass all element inside container tag
 );
-//   console.log(heading);
+console.log(container);
 // * Heading is not a html tag it is a plain javascript Object in react
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //   console.log(root);
