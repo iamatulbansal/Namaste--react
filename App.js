f6b879da9b792956b9546bac182c84b77b71fc5e
ATUL BANSAL <86524139+iamatulbansal@users.noreply.github.com>
2023-05-09 02:13:04 +0530

    we have done my hard coded card

--- App.js
+++ App.js
@@ -2,56 +2,75 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const Title = () => {
-  return (
-    <React.Fragment>
-      <a href='/' className='titleAnchorTag'>
-        <img
-          alt='LOGO'
-          className='logo'
-          src='https://obs.line-scdn.net/0m0339dd5b72513e8a8d6127b836e5ecf1e128f1da5cb7'
-        />
-      </a>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <a href='/' className='titleAnchorTag'>
+                <img
+                    alt='LOGO'
+                    className='logo'
+                    src='https://obs.line-scdn.net/0m0339dd5b72513e8a8d6127b836e5ecf1e128f1da5cb7'
+                />
+            </a>
+        </React.Fragment>
+    );
 };
 
 const Header = () => {
-  return (
-    <div className='container'>
-      <Title />
-      <ul className='nav-items'>
-        <li className='nav-list'>Home</li>
-        <li className='nav-list'>About</li>
-        <li className='nav-list'>Cart</li>
-        <li className='nav-list'>Logout</li>
-      </ul>
-    </div>
-  );
+    return (
+        <div className='container'>
+            <Title />
+            <ul className='nav-items'>
+                <li className='nav-list'>Home</li>
+                <li className='nav-list'>About</li>
+                <li className='nav-list'>Cart</li>
+                <li className='nav-list'>Logout</li>
+            </ul>
+        </div>
+    );
 };
 
+//WE ARE GOING TO CREATE RESTAURANT CARD
+const RestaurantCard = () => {
+    return (
+        <div className="restaurant-card">
+            <img src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/uvzfjwcslztz1tzjlzfg" alt="Restaurant-card-img" />
+            <strong>Ahuja Chinese Food Court</strong>
+            <p>Indian, Sweets, Beverages</p>
+            <p><b>3.5</b> STAR</p>
+            <p><b>37</b> MINS</p>
+            <p><b>₹200</b> FOR TWO</p>
+        </div>
+    )
+}
+
+
 const Body = () => {
-  return (
-    <React.Fragment>
-      <h2>Body</h2>
-    </React.Fragment>
-  );
+    return (
+        <div className="body-container">
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+        </div>
+    );
 };
 const Footer = () => {
-  return (
-    <React.Fragment>
-      <h2>Footer</h2>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <h2>Footer</h2>
+        </React.Fragment>
+    );
 };
 
 const AppLayout = () => {
-  return (
-    <React.Fragment>
-      <Header />
-      <Body />
-      <Footer />
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <Header />
+            <Body />
+            <Footer />
+        </React.Fragment>
+    );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -83,5 +102,12 @@ root.render(<AppLayout />);
  * *when we use multi element inside our components then must be wrap up inside single element JSX expressions must have one parent element
  *
  * *React.Fragment is a Component come from React core library or come from react node_modules
+ *
+ * Todo:Config Driven UI
+ * *Dynamic UI
+ * *Control UI using Data.config.json file
+ *
+ *
+ * *Props  is a Properties
  *
  * */
