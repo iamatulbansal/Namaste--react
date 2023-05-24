ffb1ca8d024ac9df1741eaf91c3be96f93b6f410
ATUL BANSAL <86524139+iamatulbansal@users.noreply.github.com>
2023-05-25 01:13:30 +0530

    refactor code

--- src/components/InstaMart.js
+++ src/components/InstaMart.js
@@ -1,65 +1,70 @@
 import React, { useState } from 'react';
 
-const Profile = () => {
-    const [isVisible, setIsVisible] = useState(false)
+const Profile = ({ isVisible,setIsVisible }) => {
     return (
         <>
-            <div className='border-2 border-indigo-600 m-2'>
-                <div className='bg-gray-200 cursor-pointer'>Profile</div>
-                {isVisible && <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi, facere
-                    laborum? Quas quo, aut dolor possimus veritatis harum assumenda porro
-                    laudantium laborum, vero tempore rem pariatur libero reprehenderit
-                    provident quaerat.{' '}
-                </p>}
-
+            <div className="border-2 border-indigo-600 m-2">
+                <div className="bg-gray-200 cursor-pointer">Profile</div>
+                {!isVisible && (
+                  <>  <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi,
+                        facere laborum? Quas quo, aut dolor possimus veritatis harum
+                        assumenda porro laudantium laborum, vero tempore rem pariatur libero
+                        reprehenderit provident quaerat.
+                    </p>     {isVisible ? <button className='bg-gray-300 px-2 m-2 rounded-lg' onClick={() => setIsVisible(null)}>Hide</button> : <button className='bg-green-300 px-2 m-2 rounded-lg' onClick={() => setIsVisible("team")}>Show</button>}
+                    </>
+                )}
             </div>
         </>
     );
 };
-const Carrier = () => {
-    const [isVisible, setIsVisible] = useState(true)
+const Carrier = ({ isVisible,setIsVisible }) => {
     return (
         <>
-            <div className='border-2 border-indigo-600 m-2'>
-                <div className='bg-gray-200 cursor-pointer'>Carrier</div>
-                {isVisible && <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi, facere
-                    laborum? Quas quo, aut dolor possimus veritatis harum assumenda porro
-                    laudantium laborum, vero tempore rem pariatur libero reprehenderit
-                    provident quaerat.{' '}
-                </p>}
-
+            <div className="border-2 border-indigo-600 m-2">
+                <div className="bg-gray-200 cursor-pointer">Carrier</div>
+                {!isVisible && (
+                    <> <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi,
+                        facere laborum? Quas quo, aut dolor possimus veritatis harum
+                        assumenda porro laudantium laborum, vero tempore rem pariatur libero
+                        reprehenderit provident quaerat.
+                    </p>
+                        {isVisible ? <button className='bg-gray-300 px-2 m-2 rounded-lg' onClick={() => setIsVisible(null)}>Hide</button> : <button className='bg-green-300 px-2 m-2 rounded-lg' onClick={() => setIsVisible("team")}>Show</button>}</>
+                )}
             </div>
         </>
     );
 };
-const Team = () => {
-
-    const [isVisible, setIsVisible] = useState(false)
+const Team = ({ isVisible, setIsVisible }) => {
     return (
         <>
-            <div className='border-2 border-indigo-600 m-2'>
-                <div className='bg-gray-200 cursor-pointer'>Team</div>
-                {isVisible && <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi, facere
-                    laborum? Quas quo, aut dolor possimus veritatis harum assumenda porro
-                    laudantium laborum, vero tempore rem pariatur libero reprehenderit
-                    provident quaerat.{' '}
-                </p>}
+            <div className="border-2 border-indigo-600 m-2">
+                <div className="bg-gray-200 cursor-pointer" >Team</div>
+                {!isVisible && (
+                    <>  <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi,
+                        facere laborum? Quas quo, aut dolor possimus veritatis harum
+                        assumenda porro laudantium laborum, vero tempore rem pariatur libero
+                        reprehenderit provident quaerat.{' '}
+                    </p>
+                        {isVisible ? <button className='bg-gray-300 px-2 m-2 rounded-lg' onClick={() => setIsVisible(null)}>Hide</button> : <button className='bg-green-300 px-2 m-2 rounded-lg' onClick={() => setIsVisible("team")}>Show</button>}
 
+                    </>
+                )}
             </div>
         </>
     );
 };
 
 const InstaMart = () => {
-    return (
-        <div className='p-2 m-2'>
+    const [isVisible, setIsVisible] = useState("")
 
-            <Profile />
-            <Carrier />
-            <Team />
+    return (
+        <div className="p-2 m-2">
+            <Profile isVisible={isVisible === "profile"} setIsVisible={setIsVisible} />
+            <Carrier isVisible={isVisible === "carrier"} setIsVisible={setIsVisible} />
+            <Team isVisible={isVisible === "team"} setIsVisible={setIsVisible} />
         </div>
     );
 };
