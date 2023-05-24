7641d222b3a419be0d05856d0ee224998e2be186
ATUL BANSAL <86524139+iamatulbansal@users.noreply.github.com>
2023-05-25 00:47:05 +0530

    make custom accordian

--- src/components/InstaMart.js
+++ src/components/InstaMart.js
@@ -1,28 +1,67 @@
 import React, { useState } from 'react';
 
-const InnerComponents = ({ name, setName }) => {
-  const [value, setValue] = useState('bansal');
+const Profile = () => {
+    const [isVisible, setIsVisible] = useState(false)
+    return (
+        <>
+            <div className='border-2 border-indigo-600 m-2'>
+                <div className='bg-gray-200 cursor-pointer'>Profile</div>
+                {isVisible && <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi, facere
+                    laborum? Quas quo, aut dolor possimus veritatis harum assumenda porro
+                    laudantium laborum, vero tempore rem pariatur libero reprehenderit
+                    provident quaerat.{' '}
+                </p>}
 
-  return (
-    <div>
-      InnerComponents:-{name}{' '}
-      <button className="bg-green-100" onClick={() => setName(value)}>
-        Update Data
-      </button>
-    </div>
-  );
+            </div>
+        </>
+    );
+};
+const Carrier = () => {
+    const [isVisible, setIsVisible] = useState(true)
+    return (
+        <>
+            <div className='border-2 border-indigo-600 m-2'>
+                <div className='bg-gray-200 cursor-pointer'>Carrier</div>
+                {isVisible && <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi, facere
+                    laborum? Quas quo, aut dolor possimus veritatis harum assumenda porro
+                    laudantium laborum, vero tempore rem pariatur libero reprehenderit
+                    provident quaerat.{' '}
+                </p>}
+
+            </div>
+        </>
+    );
+};
+const Team = () => {
+
+    const [isVisible, setIsVisible] = useState(false)
+    return (
+        <>
+            <div className='border-2 border-indigo-600 m-2'>
+                <div className='bg-gray-200 cursor-pointer'>Team</div>
+                {isVisible && <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi, facere
+                    laborum? Quas quo, aut dolor possimus veritatis harum assumenda porro
+                    laudantium laborum, vero tempore rem pariatur libero reprehenderit
+                    provident quaerat.{' '}
+                </p>}
+
+            </div>
+        </>
+    );
 };
 
 const InstaMart = () => {
-  const [name, setName] = useState('Atul');
+    return (
+        <div className='p-2 m-2'>
 
-  return (
-    <>
-      <div>Name:-{name}</div>
-
-      <InnerComponents name={name} setName={setName} />
-    </>
-  );
+            <Profile />
+            <Carrier />
+            <Team />
+        </div>
+    );
 };
 
 export default InstaMart;
