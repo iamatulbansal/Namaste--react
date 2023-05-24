c947f9fc3f80de129bd2a5ceb86e91e26ad76d02
ATUL BANSAL <86524139+iamatulbansal@users.noreply.github.com>
2023-05-25 01:50:25 +0530

    again refactor accordian code

--- src/components/InstaMart.js
+++ src/components/InstaMart.js
@@ -1,20 +1,23 @@
 import React, { useState } from 'react';
-const Section = ({ title, description }) => {
-    const [isVisible, setIsVisible] = useState(false)
+const Section = ({ isVisible, setIsVisible, title, description }) => {
     return (
         <div className='border-2 border-indigo-600 p-2 m-2 '>
             <div className='text-lg'>{title}</div>
-            {isVisible?<button className='bg-gray-200 rounded-lg px-2' onClick={()=>setIsVisible(false)}>Hide</button>:
-             <button className='bg-green-200 rounded-lg px-2' onClick={()=>setIsVisible(true)}>Show</button>}
-           
-            
+            {isVisible ? <button className='bg-gray-200 rounded-lg px-2' onClick={() => setIsVisible()}>Hide</button> :
+                <button className='bg-green-200 rounded-lg px-2' onClick={() => setIsVisible()}>Show</button>}
             <p>{isVisible && description}</p>
         </div>
     );
 };
 
 const InstaMart = () => {
-    const [isVisible, setIsVisible] = useState('');
+    const [sectionConfig, setSectionConfig] = useState({
+        About: false,
+        Team: true,
+        Careers: false
+
+
+    })
 
     return (
         <div className="p-2 m-2">
@@ -22,14 +25,43 @@ const InstaMart = () => {
             <Section
                 title="About instaMart"
                 description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem aspernatur aut sapiente eos voluptate doloremque, laborum voluptates aliquid quasi pariatur vitae! Quam voluptatum eveniet tempore eum blanditiis culpa iste eos."
+
+                isVisible={sectionConfig.About}
+                setIsVisible={() => setSectionConfig({
+                    About: true,
+                    Team: false,
+                    Careers: false
+
+
+                })}
+
             />
             <Section
                 title="Team instaMart"
                 description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem aspernatur aut sapiente eos voluptate doloremque, laborum voluptates aliquid quasi pariatur vitae! Quam voluptatum eveniet tempore eum blanditiis culpa iste eos."
+                isVisible={sectionConfig.Team}
+                setIsVisible={() => setSectionConfig({
+                    About: false,
+                    Team: true,
+                    Careers: false
+
+
+                })}
+
             />
             <Section
                 title="Careers instaMart"
                 description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem aspernatur aut sapiente eos voluptate doloremque, laborum voluptates aliquid quasi pariatur vitae! Quam voluptatum eveniet tempore eum blanditiis culpa iste eos."
+
+                isVisible={sectionConfig.Careers}
+                setIsVisible={() => setSectionConfig({
+                    About: false,
+                    Team: false,
+                    Careers: true
+
+
+                })}
+
             />
         </div>
     );
