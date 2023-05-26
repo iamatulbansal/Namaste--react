e56c2ff846d944b71492f111dfc0b17c443c1a25
ATUL BANSAL <86524139+iamatulbansal@users.noreply.github.com>
2023-05-26 15:16:08 +0530

    updated with test case first and install some npm for testing and import export feature with help of babel

--- src/components/__test__/sum.test.js
+++ src/components/__test__/sum.test.js
@@ -1,4 +1,5 @@
-const sum = require("../sum")
+// const sum = require("../sum")
+import sum from "../sum"; //we are using import export with help of babel updating our .babelrc file with preset
 
 test('Sum should be 6', () => {
     expect(sum(3, 3)).toBe(6);
